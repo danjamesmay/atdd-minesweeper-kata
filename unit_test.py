39d492805113f08446084d2368dff5c8b2b4942a
danjamesmay <danjamesmay@gmail.com>
2019-02-27 14:42:34 +0000

    Refactor tests to use parametrization

--- unit_test.py
+++ unit_test.py
@@ -1,51 +1,40 @@
 from minesweeper import generate_minefield
+import pytest
+
 
 def test_empty_minefield_description_outputs_nothing():
     output = generate_minefield("0 0")
 
     assert output is None
 
-def test_outputs_minefield_for_1x1_minefield_description_with_no_mines():
-    output = generate_minefield("""1 1
-.""")
+@pytest.mark.parametrize("minefield_description,expected_minefield", [
+    ("""1 1
+.""", "0"),
+    ("""1 2
+..""", "00"),
+    ("""2 1
+.
+.""", """0
+0"""),
+    ("""3 1
+.
+.
+.""", """0
+0
+0"""),
+    ("""2 2
+..
+..""","""00
+00""")
+])
+def test_outputs_minefield_for_n_by_m_minefield_description_with_no_mines(
+    minefield_description,
+    expected_minefield):
+    assert generate_minefield(minefield_description) == expected_minefield
 
-    assert output == "0"
 
 def test_outputs_minefield_for_1x1_minefield_description_with_one_mine():
     output = generate_minefield("""1 1
 *""")
 
     assert output == "*"
-
-
-def test_outputs_minefield_for_1x2_minefield_description_with_no_mines():
-    output = generate_minefield("""1 2
-..""")
-
-    assert output == "00"
-
-def test_outputs_minefield_for_2x1_minefield_description_with_no_mines():
-    output = generate_minefield("""2 1
-.
-.""")
-
-    assert output == """0
-0"""
-
-def test_outputs_minefield_for_3x1_minefield_description_with_no_mines():
-    output = generate_minefield("""3 1
-.
-.
-.""")
-
-    assert output == """0
-0
-0"""
-
-def test_outputs_minefield_for_2x2_minefield_description_with_no_mines():
-    output = generate_minefield("""2 2
-..
-..""")
-
-    assert output == """00
-00"""
