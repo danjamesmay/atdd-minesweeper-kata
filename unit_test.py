4f6b307db215285dd15b1b55d519d47e83cdce5b
danjamesmay <danjamesmay@gmail.com>
2019-02-27 15:05:03 +0000

    Tidy tests

--- unit_test.py
+++ unit_test.py
@@ -1,31 +1,72 @@
 from minesweeper import generate_minefield
 import pytest
-
+from textwrap import dedent
 
 def test_empty_minefield_description_outputs_nothing():
     output = generate_minefield("0 0")
 
     assert output is None
 
+def minefield_description(text):
+    return dedent(text).lstrip()
+
+def expected_minefield(text):
+    return dedent(text).strip()
+
 @pytest.mark.parametrize("minefield_description,expected_minefield", [
-    ("""1 1
-.""", "0"),
-    ("""1 2
-..""", "00"),
-    ("""2 1
-.
-.""", """0
-0"""),
-    ("""3 1
-.
-.
-.""", """0
-0
-0"""),
-    ("""2 2
-..
-..""","""00
-00""")
+    (
+        minefield_description("""
+            1 1
+            .
+        """),
+        expected_minefield("""
+            0
+        """)
+    ),
+    (
+        minefield_description("""
+            1 2
+            ..
+        """),
+        expected_minefield("""
+            00
+        """)
+    ),
+    (
+        minefield_description("""
+            2 1
+            .
+            .
+        """),
+        expected_minefield("""
+            0
+            0
+        """)
+    ),
+    (
+        minefield_description("""
+            3 1
+            .
+            .
+            .
+        """),
+        expected_minefield("""
+            0
+            0
+            0
+        """)
+    ),
+    (
+        minefield_description("""
+            2 2
+            ..
+            ..
+        """),
+        expected_minefield("""
+            00
+            00
+        """)
+    )
 ])
 def test_outputs_minefield_for_n_by_m_minefield_description_with_no_mines(
     minefield_description,
